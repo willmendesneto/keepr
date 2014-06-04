8551062b5b61c518224b16b7b6d34ee84d8babb6
root <willmendesneto@gmail.com>
2014-06-04 00:15:58 -0300

    Add new directive "kpFocus" for validate form in focus/bind events

---
+++ app/scripts/directives/focus.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Validate input element on focus/blur event
+ *
+ * @module directives
+ * @main kpFocus
+ * @static
+ */
+angular.module('keepr')
+  .directive('kpFocus', function () {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, element, controller) {
+        controller.$focused = false;
+
+        /**
+         * Activate directive on focus event
+         * @return
+         */
+        element.bind('focus', function(){
+          scope.$apply(function(){
+            controller.$focused = true;
+          });
+        })
+
+        /**
+         * Deactivate directive on blur event
+         * @return
+         */
+        .bind('blur', function(){
+          scope.$apply(function(){
+            controller.$focused = false;
+          });
+        });
+
+        /**
+         * Destroy events on "$destroy" scope event
+         * @return
+         */
+        scope.$on('$destroy', function() {
+          element.unbind('blur focus');
+        });
+      }
+    };
+  });
---
+++ test/spec/directives/focus.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Directive: kpFocus', function () {
+
+  // load the directive's module
+  beforeEach(module('keepr'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope, $compile) {
+
+    scope = $rootScope.$new();
+    scope.x = '1234567890xpto';
+    element = $compile('<input kp-focus type="number" placeholder="_" ng-model="x" required></input>')(scope);
+    scope.$digest();
+  }));
+
+  it('should have a kp-focus defined', function() {
+    expect(element).toBeDefined();
+  });
+});
