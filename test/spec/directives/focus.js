7477169f5fd2cb72ec4a66aeb900faf582449d86
willmendesneto <willmendesneto@gmail.com>
2015-03-20 15:53:26 -0300

    update tests for kp-focus directive

--- test/spec/directives/focus.js
+++ test/spec/directives/focus.js
@@ -12,11 +12,23 @@ describe('Directive: kpFocus', function () {
 
     scope = $rootScope.$new();
     scope.x = '1234567890xpto';
-    element = $compile('<input kp-focus type="text" placeholder="_" ng-model="x" required></input>')(scope);
+    element = $compile('<input kp-focus type="text" placeholder="_" ng-model="x" required />')(scope);
     scope.$digest();
   }));
 
   it('should have a kp-focus defined', function() {
     expect(element).toBeDefined();
+  });
+
+  it('should have a form field required in focus', function() {
+    spyOn(scope, '$apply');
+
+    element.val('baz');
+    element.triggerHandler('focus');
+
+    expect(scope.$apply).toHaveBeenCalled();
+
+    element.triggerHandler('blur');
+    expect(scope.$apply).toHaveBeenCalled();
   });
 });
